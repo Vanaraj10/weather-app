952c1fb83d598e7c8c34a158722c79723cb3694c
vanaraj10 <vanaraj1018@gmail.com>
2025-04-04 04:49:09 -0700

    added background img

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -2,13 +2,8 @@ import { useRef } from "react";
 import { useState } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
-import clear_icon from "../assets/clear.png";
-import rain_icon from "../assets/rain.png";
-import snow_icon from "../assets/snow.png";
-import wind_icon from "../assets/wind.png";
-import cloud_icon from "../assets/cloud.png";
 import humidity_icon from "../assets/humidity.png";
-import drizzle_icon from "../assets/drizzle.png";
+import wind_icon from "../assets/wind.png";
 
 const Weather = () => {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -19,25 +14,10 @@ const Weather = () => {
     temperature: number;
     location: string;
     icon: string;
+    description:string;
   }
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
 
-  const allIcons: { [key: string]: string } = {
-    "01d": clear_icon,
-    "01n": clear_icon,
-    "02d": cloud_icon,
-    "02n": cloud_icon,
-    "03d": cloud_icon,
-    "03n": cloud_icon,
-    "04d": drizzle_icon,
-    "04n": drizzle_icon,
-    "09d": rain_icon,
-    "09n": rain_icon,
-    "10d": rain_icon,
-    "10n": rain_icon,
-    "13d": snow_icon,
-    "13n": snow_icon,
-  };
   const search = async (city: string) => {
     if (city === ""){
         return;
@@ -47,14 +27,14 @@ const Weather = () => {
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
-      const icon =
-        allIcons[data.weather[0].icon as keyof typeof allIcons] || clear_icon;
+      const icon =data.weather[0].icon;
       setWeatherData({
         humidity: data.main.humidity,
         windSpeed: data.wind.speed,
         temperature: Math.floor(data.main.temp),
         location: data.name,
-        icon: icon,
+        icon: `https://openweathermap.org/img/wn/${icon}@2x.png`,
+        description:data.weather[0].description
       });
     } catch (error) {
        setWeatherData(null);
@@ -65,7 +45,7 @@ const Weather = () => {
 
   return (
     <div className="weather">
-        <h1>StarLight Weather ⭐</h1>
+        <h1>"StarLight Weather ⭐</h1>
       <div className="search-bar">
         <input ref={inputRef} type="text" placeholder="Enter city name " />
         <img
@@ -81,6 +61,7 @@ const Weather = () => {
             className="weather-icon"
             alt="Weather Icon"
           />
+          <p className="description">{weatherData.description}</p>
           <p className="temperature">{weatherData.temperature} °C</p>
           <p className="location">{weatherData.location}</p>
           <div className="weather-data">
